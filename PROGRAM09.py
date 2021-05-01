7a6356be4a8c280852f90846db35ff240a8dfed9
amangangwani1101 <83112402+amangangwani1101@users.noreply.github.com>
2021-05-02 00:04:50 +0530

    substring

---
+++ PROGRAM09.py
@@ -0,0 +1,8 @@
+stri=input("Enter the string:-\n")
+li=stri.split()
+if(len(li)==1):
+    print(f"STRING INPUT BY USER '{stri}' DONOT HAVE ANY SUBSTRING")
+else:
+    print(f"STRING INPUT BY USER '{stri}'  HAVE SUBSTRING")
+    
+    
