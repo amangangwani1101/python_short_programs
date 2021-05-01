ccff96d5e1f1d280b5fe7e7ad28bb503eabecadf
amangangwani1101 <83112402+amangangwani1101@users.noreply.github.com>
2021-05-01 23:57:18 +0530

    1ST

---
+++ PROGRAM_01.py
@@ -0,0 +1,17 @@
+for i in range(1,6):
+    print(" * "*i)
+for j in range(1,5):
+    e=5-j
+    print(" * "*e)
+
+# OUTPUT
+#  * 
+#  *  * 
+#  *  *  * 
+#  *  *  *  * 
+#  *  *  *  *  * 
+#  *  *  *  * 
+#  *  *  * 
+#  *  * 
+#  * 
+
